a48051367cbab6b2caf37d30536e5611ae4cd29e
Gustavo Pinoti <gustavo@welbe.mx>
2025-08-08 21:01:53 -0300

    Capitulo 4 Aula 1 - Criando um bucket do S3 - Fim

--- lib/curso-cdk-stack.ts
+++ lib/curso-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class CursoCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,6 +13,15 @@ export class CursoCdkStack extends cdk.Stack {
 
     const queue2 = new sqs.Queue(this, "CursoCdkQueue2", {
       visibilityTimeout: cdk.Duration.seconds(300),
+    });
+
+    new s3.Bucket(this, "CursoCdkBucket", {
+      bucketName: "curso-cdk-123",
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      versioned: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
     });
   }
 }
