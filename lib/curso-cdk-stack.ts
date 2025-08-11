b675ae9738ef38d9f5efb1cfb82f7d34f419fe56
Gustavo Pinoti <gustavopinoticel@gmail.com>
2025-08-10 21:23:49 -0300

    Capitulo 4 Aula 2 - Criando tabelas do Dynamo DB - Estrutura final

--- lib/curso-cdk-stack.ts
+++ lib/curso-cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class CursoCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -22,6 +23,30 @@ export class CursoCdkStack extends cdk.Stack {
       versioned: true,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.S3_MANAGED,
+    });
+
+    const table = new dynamodb.TableV2(this, "CursoCdkTable", {
+      tableName: "custo-cdk-teste",
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "data", type: dynamodb.AttributeType.STRING },
+      billing: dynamodb.Billing.onDemand(),
+      encryption: dynamodb.TableEncryptionV2.dynamoOwnedKey(),
+      globalSecondaryIndexes: [
+        {
+          indexName: "curso-cdk-index",
+          partitionKey: { name: "nome", type: dynamodb.AttributeType.STRING },
+          projectionType: dynamodb.ProjectionType.ALL,
+        },
+      ],
+      dynamoStream: dynamodb.StreamViewType.NEW_IMAGE,
+      deletionProtection: false,
+      tableClass: dynamodb.TableClass.STANDARD,
+    });
+
+    table.addGlobalSecondaryIndex({
+      indexName: "curso-cdk-index-2",
+      partitionKey: { name: "email", type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.ALL,
     });
   }
 }
