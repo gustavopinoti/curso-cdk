9270eeb424bc9e8908b0e351765242c13bc04ded
Gustavo Pinoti <gustavo@welbe.mx>
2025-08-12 20:59:57 -0300

    Capitulo 4 Aula 7 - Criando Instancia de Banco de Dados - Estrutura Final

--- lib/curso-cdk-stack.ts
+++ lib/curso-cdk-stack.ts
@@ -5,6 +5,7 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
 
 export class CursoCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -131,6 +132,27 @@ export class CursoCdkStack extends cdk.Stack {
       vpcSubnets: {
         subnets: vpc.publicSubnets,
       },
+    });
+
+    new rds.DatabaseInstance(this, "CursoCdkRds", {
+      instanceIdentifier: "curso-cdk-rds",
+      databaseName: "curso_cdk",
+      port: 3306,
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_4_5,
+      }),
+      allocatedStorage: 20,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO
+      ),
+      vpc: vpc,
+      vpcSubnets: {
+        subnets: vpc.privateSubnets,
+      },
+      multiAz: false,
+      storageType: rds.StorageType.GP2,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
   }
 }
