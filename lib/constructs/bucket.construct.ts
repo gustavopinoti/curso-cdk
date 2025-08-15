a8ad12f7011b39fd78c77ae00686bd6b90e42fa2
Gustavo Pinoti <gustavo@welbe.mx>
2025-08-14 22:04:40 -0300

    Capitulo 5 Aula 2 - Separando em Stacks - Criando a Storage Stack - Estrutura Final

---
+++ lib/constructs/bucket.construct.ts
@@ -0,0 +1,25 @@
+import { Stack } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+
+export interface BucketConstructProps {
+  bucketName: string;
+}
+
+export class BucketConstruct extends Construct {
+  constructor(scope: Stack, id: string, props: BucketConstructProps) {
+    super(scope, id);
+
+    const { bucketName } = props;
+
+    new s3.Bucket(scope, bucketName, {
+      bucketName,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      versioned: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
+  }
+}
---
+++ lib/constructs/dynamodb-table.construct.ts
@@ -0,0 +1,31 @@
+import { Stack } from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+
+export interface DynamodbTableConstructProps {
+  tableName: string;
+  sortKey?: { name: string; type: dynamodb.AttributeType };
+  globalSecondaryIndexes?: dynamodb.GlobalSecondaryIndexPropsV2[];
+}
+
+export class DynamodbTableConstruct extends Construct {
+  constructor(scope: Stack, id: string, props: DynamodbTableConstructProps) {
+    super(scope, id);
+
+    const { tableName, sortKey, globalSecondaryIndexes } = props;
+
+    new dynamodb.TableV2(scope, tableName, {
+      tableName,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      sortKey,
+      billing: dynamodb.Billing.onDemand(),
+      encryption: dynamodb.TableEncryptionV2.dynamoOwnedKey(),
+      globalSecondaryIndexes,
+      dynamoStream: dynamodb.StreamViewType.NEW_IMAGE,
+      deletionProtection: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableClass: dynamodb.TableClass.STANDARD,
+    });
+  }
+}
