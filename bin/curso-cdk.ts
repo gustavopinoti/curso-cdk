05fa7bebb8f141d8cdfe1d5525d2f6cfbcfa8272
Gustavo Pinoti <gustavo@welbecare.com>
2025-09-02 23:02:06 -0300

    Capitulo 5 Aula 4 - Separando em Stacks - Criando a Application Stack - Estrutura Final

--- bin/curso-cdk.ts
+++ bin/curso-cdk.ts
@@ -3,20 +3,22 @@ import * as cdk from "aws-cdk-lib";
 import { NetworkStack } from "../lib/network-stack";
 import { StorageStack } from "../lib/storage-stack";
 import { MessagingStack } from "../lib/messaging-cdk-stack";
+import { ApplicationStack } from "../lib/application-stack";
 
 const app = new cdk.App();
 
 const env = { account: "870140859659", region: "us-east-2" };
-
-// new CursoCdkStack(app, "CursoCdkStack", {
-//   env,
-// });
 
 const networkStack = new NetworkStack(app, "NetworkStack", {
   env,
 });
 
 new StorageStack(app, "StorageStack", {
+  env,
+  vpc: networkStack.vpc,
+});
+
+new ApplicationStack(app, "ApplicationStack", {
   env,
   vpc: networkStack.vpc,
 });
---
+++ lib/application-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+
+interface ApplicationStackProps extends cdk.StackProps {
+  vpc: ec2.Vpc;
+}
+
+export class ApplicationStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ApplicationStackProps) {
+    super(scope, id, props);
+
+    const { vpc } = props;
+
+    new ec2.Instance(this, "CursoCdkInstance", {
+      instanceName: "instancia-curso-cdk",
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.NANO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      vpc: vpc,
+      vpcSubnets: {
+        subnets: vpc.publicSubnets,
+      },
+    });
+  }
+}
