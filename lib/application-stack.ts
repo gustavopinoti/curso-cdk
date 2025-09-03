6da4348c6aa4b2ba92de077818a4b5e2bc863042
Gustavo Pinoti <gustavo@welbecare.com>
2025-09-02 23:34:15 -0300

    Capitulo 6 Aula 1 - Codigo primeiro lambda e dependencias - Estrutura Inicial

--- lib/application-stack.ts
+++ lib/application-stack.ts
@@ -3,26 +3,26 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 
 interface ApplicationStackProps extends cdk.StackProps {
-  vpc: ec2.Vpc;
+  // vpc: ec2.Vpc;
 }
 
 export class ApplicationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: ApplicationStackProps) {
     super(scope, id, props);
 
-    const { vpc } = props;
+    // const { vpc } = props;
 
-    new ec2.Instance(this, "CursoCdkInstance", {
-      instanceName: "instancia-curso-cdk",
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T3,
-        ec2.InstanceSize.NANO
-      ),
-      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
-      vpc: vpc,
-      vpcSubnets: {
-        subnets: vpc.publicSubnets,
-      },
-    });
+    // new ec2.Instance(this, "CursoCdkInstance", {
+    //   instanceName: "instancia-curso-cdk",
+    //   instanceType: ec2.InstanceType.of(
+    //     ec2.InstanceClass.T3,
+    //     ec2.InstanceSize.NANO
+    //   ),
+    //   machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+    //   vpc: vpc,
+    //   vpcSubnets: {
+    //     subnets: vpc.publicSubnets,
+    //   },
+    // });
   }
 }
