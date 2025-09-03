54c62e9abd85c8c04177cad2d042f9dbf6697c5b
Gustavo Pinoti <gustavo@welbecare.com>
2025-09-03 14:45:38 -0300

    Capitulo 6 Aula 5 - Codigo lambda com envio SQS e SNS - Estrutura Inicial

--- src/handlers/publica-topico-fila/publica-topico-fila.handler.ts
+++ src/handlers/publica-topico-fila/publica-topico-fila.handler.ts
@@ -8,12 +8,46 @@ export async function handler(event: any): Promise<any> {
   });
 
   const command = new PublishCommand({
-    Message: JSON.stringify({ message: "Hello from SNS!" }),
+    Message: JSON.stringify({ message: "Hello from SNS!", type: "course-1" }),
     TopicArn: process.env.TOPIC_ARN,
+    MessageAttributes: {
+      versao: {
+        DataType: "String",
+        StringValue: "1",
+      },
+    },
   });
 
   await snsClient.send(command);
-  console.log("Mensagem publicada no SNS");
+  console.log("Mensagem publicada no SNS - Curso - Versao 1");
+
+  const command2 = new PublishCommand({
+    Message: JSON.stringify({ message: "Hello from SNS!", type: "item-1" }),
+    TopicArn: process.env.TOPIC_ARN,
+    MessageAttributes: {
+      versao: {
+        DataType: "String",
+        StringValue: "2",
+      },
+    },
+  });
+
+  await snsClient.send(command2);
+  console.log("Mensagem publicada no SNS - Item - Versao 2");
+
+  const command3 = new PublishCommand({
+    Message: JSON.stringify({ message: "Hello from SNS!", type: "item-1" }),
+    TopicArn: process.env.TOPIC_ARN,
+    MessageAttributes: {
+      versao: {
+        DataType: "String",
+        StringValue: "3",
+      },
+    },
+  });
+
+  await snsClient.send(command3);
+  console.log("Mensagem publicada no SNS - Item - Versao 3");
 
   const sqsClient = new SQSClient({
     region: "us-east-2",
@@ -21,9 +55,23 @@ export async function handler(event: any): Promise<any> {
 
   const sqsCommand = new SendMessageCommand({
     QueueUrl: process.env.QUEUE_URL,
-    MessageBody: JSON.stringify({ message: "Hello from SQS!" }),
+    MessageBody: JSON.stringify({
+      message: "Hello from SQS!",
+      status: "ativo",
+    }),
   });
 
   await sqsClient.send(sqsCommand);
-  console.log("Mensagem publicada no SQS");
+  console.log("Mensagem publicada no SQS Ativa");
+
+  const sqsCommand2 = new SendMessageCommand({
+    QueueUrl: process.env.QUEUE_URL,
+    MessageBody: JSON.stringify({
+      message: "Hello from SQS!",
+      status: "inativo",
+    }),
+  });
+
+  await sqsClient.send(sqsCommand2);
+  console.log("Mensagem publicada no SQS Inativa");
 }
