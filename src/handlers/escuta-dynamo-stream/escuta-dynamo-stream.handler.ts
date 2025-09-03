a7b26b72db37fdc22b1a760d86a03e88efbc40ac
Gustavo Pinoti <gustavo@welbecare.com>
2025-09-03 17:07:48 -0300

    Capitulo 6 Aula 7 - Codigo lambda escuta DynamoDB Stream - Estrutura Inicial

---
+++ src/handlers/escuta-dynamo-stream/escuta-dynamo-stream.handler.ts
@@ -0,0 +1,9 @@
+import { DynamoDBStreamEvent, S3Event } from "aws-lambda";
+
+export async function handler(event: DynamoDBStreamEvent): Promise<void> {
+  console.log("Lambda triggado pelo DynamoDb Stream");
+  console.log(
+    `Evento: ${event.Records[0].eventName}`,
+    event.Records[0].dynamodb
+  );
+}
