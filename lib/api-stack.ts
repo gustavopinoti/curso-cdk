17638f7de1fe4b766f15d200176872e99c0a2640
Gustavo Pinoti <gustavopinoticel@gmail.com>
2025-09-03 23:55:20 -0300

    Capitulo 7 Aula 4 - Criando API Key - Estrutura Final

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -22,15 +22,42 @@ export class ApiStack extends cdk.Stack {
         allowMethods: apigateway.Cors.ALL_METHODS,
         allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
       },
+      apiKeySourceType: apigateway.ApiKeySourceType.HEADER,
     });
+
+    const apiKey = api.addApiKey("curso-api-key", {
+      apiKeyName: "curso-api-key",
+      description: "Chave para acessar a API do curso",
+    });
+
+    const plan = api.addUsagePlan("CursoUsagePlan", {
+      name: "curso-usage-plan",
+      apiStages: [
+        {
+          api,
+          stage: api.deploymentStage,
+        },
+      ],
+    });
+    plan.addApiKey(apiKey);
 
     const lambda = new LambdaConstruct(this, {
       functionName: "primeira-api",
       entry: "handlers/primeira-api/primeira-api.handler.ts",
     }).lambda;
 
-    const helloResource = api.root.addResource("hello");
-    helloResource.addMethod("GET", new apigateway.LambdaIntegration(lambda));
+    const helloResource = api.root.addResource("hello", {
+      defaultMethodOptions: {
+        apiKeyRequired: true,
+      },
+    });
+    helloResource.addMethod("GET", new apigateway.LambdaIntegration(lambda), {
+      apiKeyRequired: false,
+    });
     helloResource.addMethod("POST", new apigateway.LambdaIntegration(lambda));
+
+    helloResource
+      .addResource("test-key")
+      .addMethod("GET", new apigateway.LambdaIntegration(lambda));
   }
 }
