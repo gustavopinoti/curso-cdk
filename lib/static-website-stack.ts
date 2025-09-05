fc6c37a83f8dc2fa7f8294297dead0c53c6d8684
Gustavo Pinoti <gustavopinoticel@gmail.com>
2025-09-05 11:39:51 -0300

    Capitulo 9 Aula 2 - Criando CloudFront distribution - Estrutura Final

--- lib/static-website-stack.ts
+++ lib/static-website-stack.ts
@@ -3,6 +3,8 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as cloudfrontorigins from "aws-cdk-lib/aws-cloudfront-origins";
 
 interface StaticWebsiteStackProps extends cdk.StackProps {}
 
@@ -17,9 +19,36 @@ export class StaticWebsiteStack extends cdk.Stack {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
 
-    new s3Deploy.BucketDeployment(this, "static-website-bucket-deploy", {
+    const distribution = new cloudfront.Distribution(this, "distribution", {
+      defaultBehavior: {
+        origin:
+          cloudfrontorigins.S3BucketOrigin.withOriginAccessIdentity(
+            websiteBucket
+          ),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        originRequestPolicy: cloudfront.OriginRequestPolicy.CORS_S3_ORIGIN,
+        responseHeadersPolicy:
+          cloudfront.ResponseHeadersPolicy.SECURITY_HEADERS,
+      },
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: "/index.html",
+          ttl: cdk.Duration.days(1),
+        },
+      ],
+      defaultRootObject: "index.html",
+      httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+    });
+
+    new s3Deploy.BucketDeployment(this, "DeployWebsite", {
       sources: [s3Deploy.Source.asset("./front")],
       destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ["/*"],
     });
   }
 }
